afe2465f32459f5f7c97b6777e0a65be5e42c75d
mavlon00 <mavlon90d5@gmail.com>
2026-01-14 11:04:33 +0100

    I leanrt about strings ( adding strings together, multiline strings, strings with quotation mark )

---
+++ strings.py
@@ -0,0 +1,21 @@
+  # strings
+  #
+  # 
+  #  
+my_string = "hello, world!"
+print(my_string)
+
+nother_with_quotes = "hello, it's me" # print with quotation mark
+ 
+ # multiline print
+multiline = """"
+hello, world. 
+my name is sammy. welcome to my program
+"""
+print(multiline)
+
+# adding string togerther 
+name = "sammy."
+greeting = "hello, " + name
+print(greeting)
+
