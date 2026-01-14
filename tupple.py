97979f984a983b231c1983f4513cb6302dc97803
mavlon00 <mavlon90d5@gmail.com>
2026-01-14 15:54:42 +0100

    I learnt how to use tuple and the difference between tuple and list

---
+++ tupple.py
@@ -0,0 +1,7 @@
+short_tuple = "sammy", "paul"
+a_bit_clearer = ("sammy", "paul")
+tuple_in_list = [("sammy", "paul")]
+
+friends = ("sammy", "paul", "ugo")
+friends = friends + ("james",)
+print(friends)
