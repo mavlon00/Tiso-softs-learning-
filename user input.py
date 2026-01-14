ec0cf36783399f25f4b64b79a59c6dc69fa09c7e
mavlon00 <mavlon90d5@gmail.com>
2026-01-14 12:32:40 +0100

    I learnt how to get user input

---
+++ user input.py
@@ -0,0 +1,15 @@
+age = int(input("Enter you age; "))
+months = age * 12
+print(f"you have lived for {months} months. ") 
+
+
+# excercise( how many seconds a user have lived )
+
+age = int(input("Enter you age: "))
+seconds = age * 31,536,000
+print(f"you have live for {seconds} seconds. ")
+
+
+
+
+
